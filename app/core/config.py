febfd64172ebb973bbd4249a6b33327142ec4722
Darkhan <211312@astanait.edu.kz>
2025-01-10 02:34:21 +0500

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5431/elearn_db"
+    DATABASE_URL: str = "postgresql://postgres:RCiigvdJZYLECNiDbcBXwWodatGCNSFA@roundhouse.proxy.rlwy.net:43532/railway"
     SECRET_KEY: str = "supersecretkey"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_HOURS: int = 24
